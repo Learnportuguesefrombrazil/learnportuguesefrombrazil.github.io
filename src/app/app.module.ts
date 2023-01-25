261d0fd4e2daac40e8b8a6a668f6f83241972d06
jonataslaet <jonataslaetprogramador@gmail.com>
2023-01-25 18:32:07 -0300

    Correção de bug

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+import { AboutUsComponent } from './about-us/about-us.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { HomeComponent } from './home/home.component';
-import { FooterComponent } from './footer/footer.component';
-import { AboutUsComponent } from './about-us/about-us.component';
 import { rotas } from './app.rotas';
-import { PortugueseArticleComponent } from './portuguese-article/portuguese-article.component';
 import { EnglishArticleComponent } from './english-article/english-article.component';
-
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PortugueseArticleComponent } from './portuguese-article/portuguese-article.component';
 
 @NgModule({
   declarations: [
@@ -21,14 +21,14 @@ import { EnglishArticleComponent } from './english-article/english-article.compo
     AboutUsComponent,
     PortugueseArticleComponent,
     EnglishArticleComponent,
-    
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     rotas
   ],
-  providers: [],
+  providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
