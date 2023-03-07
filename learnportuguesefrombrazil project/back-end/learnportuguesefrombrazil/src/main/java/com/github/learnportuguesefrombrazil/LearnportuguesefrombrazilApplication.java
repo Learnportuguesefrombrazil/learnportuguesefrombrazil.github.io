1673326d4e53132b8ce72c19a26b0a18a76776bf
learnportuguesefrombrazil <portugueseteacherdf@gmail.com>
2023-03-07 20:44:52 -0300

    Primeiro commit projeto criado

---
+++ learnportuguesefrombrazil project/back-end/learnportuguesefrombrazil/src/main/java/com/github/learnportuguesefrombrazil/LearnportuguesefrombrazilApplication.java
@@ -0,0 +1,13 @@
+package com.github.learnportuguesefrombrazil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnportuguesefrombrazilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnportuguesefrombrazilApplication.class, args);
+	}
+
+}
---
+++ learnportuguesefrombrazil project/back-end/learnportuguesefrombrazil/src/test/java/com/github/learnportuguesefrombrazil/LearnportuguesefrombrazilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.learnportuguesefrombrazil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnportuguesefrombrazilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
