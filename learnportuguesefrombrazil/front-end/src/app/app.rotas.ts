2710d7765f151aed481df80c41923143463e5b67
jonataslaet <jonataslaetprogramador@gmail.com>
2023-03-07 21:47:54 -0300

    Organização de components
    Todos os components agora estão dentro de uma pasta chamada components

--- learnportuguesefrombrazil/front-end/src/app/app.rotas.ts
+++ learnportuguesefrombrazil/front-end/src/app/app.rotas.ts
@@ -1,16 +1,16 @@
 import { ModuleWithProviders } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
-import { AboutUsComponent } from "./about-us/about-us.component";
-import { EnglishArticleComponent } from "./english-article/english-article.component";
-import { HomeComponent } from "./home/home.component";
-import { PortugueseArticleComponent } from "./portuguese-article/portuguese-article.component";
+import { AboutUsComponent } from "./components/about-us/about-us.component";
+import { EnglishArticleComponent } from "./components/english-article/english-article.component";
+import { HomeComponent } from "./components/home/home.component";
+import { PortugueseArticleComponent } from "./components/portuguese-article/portuguese-article.component";
 
 const APP_MINHAS_ROTAS: Routes = [
     { path: '', component: HomeComponent},
     { path: 'portuguese-article', component: PortugueseArticleComponent},
     { path: 'english-article', component: EnglishArticleComponent},
     { path: 'about-us', component: AboutUsComponent},
-   
+
 ];
 
 export const rotas: ModuleWithProviders<RouterModule> = RouterModule.forRoot(APP_MINHAS_ROTAS);
